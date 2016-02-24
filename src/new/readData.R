b876ef06633dc519f31525cc6facadeeaafa9771
dima <dimaguks@gmail.com>
2016-02-24 15:41:11 +0200

    добавление коментариев в readTable.R

--- src/new/readData.R
+++ src/new/readData.R
@@ -1,6 +1,14 @@
+# config csv file
+#   1. name -имя пользователя
+#   2. password - пароль к базе
+#   3. host - host базы данных
+#   4. bdName - имя базы данных
+#   5. route - путь куда сохранять картинки
+#   6. myDir - текущая директория
+
+
 
 {
-  
   FormatDate = "%Y-%m-%d %H:%M";
   myBDUser = as.character(config[1,1]);
   myBDPassword = as.character(config[1,2]);
@@ -9,13 +17,12 @@
   myRoute = as.character(config[1,5]);
   myCol = "red";
   mySize = c(960, 960);
-}  
+}#  считываес параметры с config
 
 
 library(RMySQL)
 
-readTable <- function(query)
-{
+readTable <- function(query){
   con <- dbConnect(MySQL(),
                    user = myBDUser,
                    password = myBDPassword,
@@ -27,20 +34,22 @@ readTable <- function(query)
   q<-dbDisconnect(con);
   return(res);
   
-}#считывание данных из базы данных по запросу
+}#  функция для считывания таблицы из базы по завросу
 
-checkTable <- function(data)
-{
+
+checkTable <- function(data){
   return(nrow(data)>10)
   
-}# функция проверки наличия данных
+}# функция для проверки наличия нежного количества данных
+
 
 change.publish <-function(publish=data.publish){
   
   return(publish);
-}
+}# функция для обработки таблицыpublish
+
 
 change.sold <-function(sold=data.sold){
   
   return(sold);
-}
+}# функция для обработки таблицы sold
