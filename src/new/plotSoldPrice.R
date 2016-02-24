43b55a5706fb5284cc7a87fde6a909c1dd8303b4
dima <dimaguks@gmail.com>
2016-02-24 16:06:16 +0200

    немного изменил publishPrice и добавил soldPrice

--- src/new/plotSoldPrice.R
+++ src/new/plotSoldPrice.R
@@ -1,5 +1,6 @@
 # какие столбцы
-#   publish:  ProductID price_real shipping_real
+#   publish:  ItemID ProductID price_real shipping_real
+#   sold: ItemID
 
 # параметры которые нужно передавать
 #   1. путь у config
@@ -36,47 +37,59 @@ setwd(myDir);
 # запускаем скрипт с функциями считывания таблиц
 source("readData.R");
 
-# создаем запрос
-queryPublish =paste("select publish.price_real, publish.shipping_real ",
-                 "from ", bdName, ".publish", ", ", bdName, ".products ",
-                 "where publish.ProductID=products.ProductID and products.brand = '", myBrand, "' and products.ebaycategory_id = ", category, 
-                 "and publish.add_date >",begDate, " and publish.add_date < ", endDate, ";", sep = "");
+# создаем запрос для publish
+queryPublish = paste("select publish.ItemID, publish.price_real, publish.shipping_real ",
+                    "from ", bdName, ".publish", ", ", bdName, ".products ",
+                    "where publish.ProductID=products.ProductID and products.brand = '", myBrand, "' and products.ebaycategory_id = ", category, 
+                    "and publish.add_date >",begDate, " and publish.add_date < ", endDate, ";", sep = "");
+# запрос для sold
+querySold = paste("select sold.ItemID ",
+                   "from ", bdName, ".sold ",
+                   "where sold.ItemID in (select publish.ItemID from ",bdName, ".publish, ", bdName, ".products where publish.ProductID = products.ProductID and products.brand = '", myBrand,"' and products.ebaycategory_id = ", category, 
+                   "and publish.add_date >",begDate, " and publish.add_date < ", endDate, ");", sep = "");
 
 # считываем таблицу
 data.publish <- readTable(queryPublish);
+data.sold <- readTable(querySold);
 
-
-if(!checkTable(data.publish))
+if(!checkTable(data.publish) || !checkTable(data.sold))
 {
   print("ERROR");
 }#  если в таблице не достаточно элементов тогда пишем ERROR
 
 # если достаточно элементов тогда рисуем гистограмму
-if(checkData(data.publish))
+if(checkData(data.publish) & checkTable(data.sold))
 {
   # функция обработки таблицы
   data.publish = change.publish();
+  data.sold = change.sold();
   
   # созлаем точки на очи X гистограммы
   maxPrice = log10(max(data.publish$price_real+data.publish$shipping_real,na.rm = T))+0.2;
   myBreaks = seq(0, maxPrice, by = 0.1);
   
   
-  plotPublishPrice <- function(publish = data.publish, 
-                          colHist = myCol,
-                          waySave = myRoute,
-                          prof = myProf,
-                          size = mySize){
+  plotSoldPrice <- function(publish = data.publish,
+                            sold = data.sold,
+                            colHist = myCol,
+                            waySave = myRoute,
+                            prof = myProf,
+                            size = mySize){
     
-    png(file=paste0(waySave, paste0("plotPublishPrice", ".png")),width = size[1], height = size[2]);
+    soldPrice = merge(subset(sold, select = c(ItemID)), 
+                      subset(publish, select = c(ItemID, price_real, shipping_real)),
+                      by.x = "ItemID", 
+                      by.y = "ItemID");
+    
+    png(file=paste0(waySave, paste0("plotSoldPrice", ".png")),width = size[1], height = size[2]);
     
     
-    data.push_price_hist = hist(log10(publish$price_real+publish$shipping_real),
+    data.push_price_hist = hist(log10(soldPrice$price_real+soldPrice$shipping_real),
                                 freq = FALSE,
                                 breaks = myBreaks, 
                                 col = colHist,
                                 labels = TRUE,
-                                main = "Гистограмма цены выставленых товаров",
+                                main = "Гистограмма цены проданых товаров",
                                 xlab = "Log10(Price)");
     lines(density(log10(publish$price_real+publish$shipping_real)),
           col="blue",
@@ -89,6 +102,6 @@ if(checkData(data.publish))
     return("plotPublishPrice");
   }#  функция постороения гистограммы которая возвращает имя 
   
-  plotPublishPrice();# вызов функции построения гистограммы
+  plotSoldPrice();# вызов функции построения гистограммы
   
 }
